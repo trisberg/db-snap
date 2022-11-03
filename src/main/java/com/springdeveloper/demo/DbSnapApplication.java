adbd5a15bc4da9148985ce381a2ae96b13474ee6
Thomas Risberg <risbergt@vmware.com>
2022-11-03 15:19:14 -0400

    Add demo app

---
+++ src/main/java/com/springdeveloper/demo/DbSnapApplication.java
@@ -0,0 +1,13 @@
+package com.springdeveloper.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbSnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdeveloper/demo/DbSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdeveloper.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
